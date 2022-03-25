5124c4c301447da644026862142f932863906f9e
tableonthewall <seydaozdemir07@gmail.com>
2022-03-25 23:54:25 +0300

    algo exercise

--- src/main/java/a_basic/exercise/Exercise/AlgoExercise.java
+++ src/main/java/a_basic/exercise/Exercise/AlgoExercise.java
@@ -2,11 +2,21 @@ package a_basic.exercise.Exercise;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 public class AlgoExercise {
-    //1. Counting duplicate characters
+
     public static void main(String[] args) {
 
     }
+
+    //1. Counting duplicate characters
+    private static Map<Character,Integer> countingDublicate(String s){
+        return null;
+    }
+
     //2.Finding the first non-repeated character
+    private static char firstNonRepeated(String s){
+        return 'a';
+    }
 }
