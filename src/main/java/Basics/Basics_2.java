55431aad45f6f422457f6730d18d44731c055547
Seyda Özdemir <seydaozdemir07@gmail.com>
2022-11-16 21:53:19 +0300

    java basics

--- src/main/java/Basics/Basics_2.java
+++ src/main/java/Basics/Basics_2.java
@@ -108,14 +108,21 @@ public class Basics_2 {
 //                System.out.println("sonuc:"+(value*value));
 //        }
 
-        for(int i=1;i<1000;i++){
-            if(i%5!=0) {
-                //System.out.println("döngü bitti");
-                continue;//devam et
-            }else{
-                System.out.println(i);
-            }
+//        for(int i=1;i<1000;i++){
+//            if(i%5!=0) {
+//                //System.out.println("döngü bitti");
+//                continue;//devam et
+//            }else{
+//                System.out.println(i);
+//            }
+//
+//        }
 
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++){
+                System.out.print((i*j)+" ");
+            }
+            System.out.println();
         }
 
 
